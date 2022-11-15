1525f459a1ddc4b0f0eb55920921d18957a941e4
W.Y <861798495@qq.com>
2022-11-15 21:40:21 +0800

    update XimalayaFMCrawler

--- XimalayaFM/XimalayaFMCrawler.py
+++ XimalayaFM/XimalayaFMCrawler.py
@@ -5,10 +5,10 @@
 import csv
 import json
 import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import pandas as pd
 import requests
-from concurrent.futures import ThreadPoolExecutor
 
 
 class BaseXimalayaFM:
@@ -31,10 +31,17 @@ class BaseXimalayaFM:
             'perPage': 50,
             'useCache': 'false'
         }
-        if category_code == 'youshengshu' and filters:
-            params['meta'] = '-'.join([crawler._format_filters(k, v) for k, v in filters.items()])
-        cate_response = requests.get(cate_url, headers=self.headers, params=params)
-        return json.loads(cate_response.text)
+        if filters and category_code == 'youshengshu':
+            # only 'youshengshu' can be filtered by announcer and serialize state (probably)
+            params['meta'] = '-'.join([self._format_filters(k, v) for k, v in filters.items()])
+        if filters and category_code != 'youshengshu':
+            # most categories can be filtered by paid type
+            params['meta'] = self._format_filters('paid', filters['paid'])
+        if 'metadataValues not find' in requests.get(cate_url, headers=self.headers, params=params).text:
+            # some categories have no filters
+            del params['meta']
+
+        return json.loads(requests.get(cate_url, headers=self.headers, params=params).text)
 
     def get_json(self, url: str):
         response = requests.get(url, headers=self.headers)
@@ -72,15 +79,15 @@ class BaseXimalayaFM:
     def _get_category_page(self, category: str, subcategory: str = None, filters: str = None):
         total = self.get_category(page=1, category=category, subcategory=subcategory,
                                   filters=filters)['data']['total']
-        return total // 50 + 1
+        total_pages = total // 50 + 1
+        return total_pages if total_pages < 50 else 50  # maximum 50 pages for each category
 
     def _format_category(self, category: str, subcategory: str = None):
         categories = self._get_categories_map()
         # categories = pd.read_csv(r'XimalayaFM/dict_categories.csv')
         cate = categories[['category_name', 'category_code']]
         if category.lower() not in cate.values:
-            print(f'ERROR: category {category} not exist! Please check!')
-            return False
+            raise ValueError(f'ERROR: category {category} not exist! Please check!')
         category_code = (category.lower() if cate.columns[cate.eq(category.lower()).any()][0] == 'category_code'
                          else cate.loc[cate['category_name'] == category, 'category_code'].iloc[0])
         if not subcategory:
@@ -88,8 +95,7 @@ class BaseXimalayaFM:
         _ = categories.loc[categories['category_code'] == category_code].reset_index(drop=True)
         subcate = _[['subcategory_name', 'subcategory_code']]
         if subcategory.lower() not in subcate.values:
-            print(f'ERROR: subcategory {subcategory} not exist in category {category}! Please check!')
-            return False
+            raise ValueError(f'ERROR: subcategory {subcategory} not exist in category {category}! Please check!')
         subcate_code = (
             subcategory.lower() if subcate.columns[subcate.eq(subcategory.lower()).any()][0] == 'subcategory_code'
             else subcate.loc[subcate['subcategory_name'] == subcategory, 'subcategory_code'].iloc[0])
@@ -103,9 +109,8 @@ class BaseXimalayaFM:
             'paid': {'no': '132_2722', 'yes': '132_2721'}  # is paid
         }
         if not filter_map.get(key.lower()):
-            print("ERROR: invalid filter! 'announcer' should be 'single', 'double',or 'multiple'; "
-                  "'finished' should be 'no' or 'yes'; 'paid' should be 'no' or 'yes' ")
-            return False
+            raise ValueError("ERROR: invalid filter! 'announcer' should be 'single', 'double',or 'multiple'; "
+                             "'finished' should be 'no' or 'yes'; 'paid' should be 'no' or 'yes' ")
         return filter_map[key.lower()][value]
 
 
@@ -120,9 +125,9 @@ class XimalayaFMParser(BaseXimalayaFM):
         for album in category_json['data']['albums']:
             albums.append({
                 'album_id': album['albumId'],
-                'album_paid': album['isPaid'],  # 是否付费。True=需会员或付费，False=免费
-                'album_finished': album['isFinished'],  # 是否完结。0=无此属性，1=连载，2=完结
-                'album_vipType': album['vipType'],  # 付费类型。0=仅付费，1=仅会员，2=会员或付费
+                'album_paid': album['isPaid'],  # is paid. True = need paid or VIP, False = free
+                'album_finished': album['isFinished'],  # is finished. 0 = not available, 1 = serialized, 2 = finished
+                'album_vipType': album['vipType'],  # paid type. 0 = only paid, 1 = only VIP, 2 = VIP or paid
             })
         return albums
 
@@ -134,10 +139,11 @@ class XimalayaFMParser(BaseXimalayaFM):
                'album_title': album['title'],
                'album_subtitle': album['customSubTitle'],
                'album_info': album['intro'],
+               # 'album_finished': album['serializeStatus'],
                'album_tags': album.get('tags'),
                'album_cover': album['coverSmall'].split('!')[0],
-               'album_score': album.get('score'),  # 免费专辑无此属性
-               'album_score_10': self._get_album_score(album['albumId']),
+               'album_score': album.get('score'),  # quality score (user evaluation, 0-5). not available for free albums
+               'album_score_10': self._get_album_score(album['albumId']),  # popularity score (0-10). shown on the page
                'album_create': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(album['createdAt'] / 1000)),
                'album_tracks': album['tracks'],
                'album_plays': album['playTimes'],
@@ -169,14 +175,14 @@ class XimalayaFMParser(BaseXimalayaFM):
                     'track_likes': track['likes'],
                     'track_comments': track['comments'],
                     'track_create': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(track['createdAt'] / 1000)),
-                    'track_audio': track['playUrl32']
+                    'track_audio': track['playUrl32']  # download url for free (trial) tracks. valid for hours.
                 }
                 track_details.append(track_detail)
         return track_details
 
     def _get_album_score(self, album_id):
         score_url = f'https://www.ximalaya.com/revision/comment/albumStatistics/{album_id}'
-        return self.get_json(score_url)['data'].get('albumScore')  # 10分制得分，页面显示的得分
+        return self.get_json(score_url)['data'].get('albumScore')  # popularity score. 0-10
 
     def _parse_album_price(self, album_id):
         price_url = f'https://www.ximalaya.com/revision/bdsp/album/pay/schema?id={album_id}&productType=1'
@@ -190,9 +196,9 @@ class XimalayaFMParser(BaseXimalayaFM):
             album_price = None
 
         return {
-            'album_paid_type': price['paidType'],  # 价格类型。0=vip专享或者免费（无价格），1=整张购买，2=分集购买
+            'album_paid_type': price['paidType'],  # paid type. 0 = VIP or free (no price), 1 = by album, 2 = by track
             'album_price': album_price,
-            'album_price_single': (None if not price.get('retailAlbum') else price['retailAlbum']['singlePrice'])
+            'album_price_single': None if not price.get('retailAlbum') else price['retailAlbum']['singlePrice']
         }
 
     def _parse_author(self, author_id):
@@ -217,8 +223,8 @@ class XimalayaFMParser(BaseXimalayaFM):
         verify_url = f'https://m.ximalaya.com/m-revision/page/anchor/queryAnchorPage/{author_id}'
         verify = self.get_json(verify_url)['data']['anchorInfo']['userInfo']
         return {
-            'author_verified': verify['verifyStatus'],
-            'author_verified_type': verify['verifyType'],
+            'author_verified': verify['verifyStatus'],  # is verified. 1 = not verified, 3 = verified
+            'author_verified_type': verify['verifyType'],  # verified type. 1 = person, 2 = company
             'author_verified_desc': verify.get('ptitle')
         }
 
@@ -231,48 +237,60 @@ class XimalayaFMCrawler(BaseXimalayaFM):
 
     def crawler_category(self, output_file: str, category: str, subcategories: list = None,
                          filters: dict = None, pages: int = None, threads: int = 10):
+        print(f' start to crawl albums from category {category} '.center(100, '='))
+        results = []
         with ThreadPoolExecutor(threads) as t:
             for subcategory in subcategories:
-                total_pages = self._get_category_page(category=category, subcategory=subcategory, filters=filters)
-                for page in range(1, pages + 1 if pages and pages <= total_pages else total_pages + 1):
-                    t.submit(self._crawl_category, category=category, subcategory=subcategory,
-                             filters=filters, page=page, output_file=output_file)
+                max_pages = self._get_category_page(category=category, subcategory=subcategory, filters=filters)
+                for page in range(1, pages + 1 if pages and pages <= max_pages else max_pages + 1):
+                    # maximum 50 pages for each category
+                    results.append(t.submit(self._crawl_category, category=category, subcategory=subcategory,
+                                            filters=filters, page=page, output_file=output_file))
+                    total_results = as_completed(results)
+        return pd.DataFrame(sum([result.result() for result in total_results], []))
 
     def _crawl_category(self, output_file: str, category: str, subcategory: str = None,
                         filters: dict = None, page: int = None):
         albums = self.get_category(category=category, subcategory=subcategory,
                                    filters=filters, page=page)
-        albums_result = [{**album, **{'subcategory': subcategory}}
+        albums_result = [{**album, **{'category': category, 'subcategory': subcategory}}
                          for album in self.parser.parse_category(albums)]
         self.save_csv(albums_result, output_file=output_file)
         print(f'Successfully save result to {output_file} from category: {category}, {subcategory}, page: {page}')
+        return albums_result
 
-    def crawler_album(self, album_id_list: list, output_file: str, threads: int = 10):
+    @staticmethod
+    def crawler_details(func, album_id_list: list, output_file: str, threads: int = 10):
+        print(f' start to run {func.__name__} '.center(100, '='))
+        results = []
         with ThreadPoolExecutor(threads) as t:
             for album_id in album_id_list:
-                t.submit(self._crawl_album, album_id=album_id, output_file=output_file)
+                results.append(t.submit(func, album_id=album_id, output_file=output_file))
+                total_results = as_completed(results)
+        return pd.DataFrame(sum([result.result() for result in total_results], []))
 
-    def _crawl_album(self, album_id, output_file: str):
+    def crawl_album(self, album_id, output_file: str):
+        time.sleep(1)
         album_details = self.parser.parse_album(album_id)
         self.save_csv(album_details, output_file=output_file)
         print(f'Successfully save album details to {output_file} from album: {album_id}')
+        return album_details
 
-    def crawler_track(self, album_id_list: list, output_file: str, threads: int = 10):
-        with ThreadPoolExecutor(threads) as t:
-            for album_id in album_id_list:
-                t.submit(self._crawl_track, album_id=album_id, output_file=output_file)
-
-    def _crawl_track(self, album_id, output_file: str):
+    def crawl_track(self, album_id, output_file: str):
+        time.sleep(1)
         track_details = self.parser.parse_track(album_id)
         self.save_csv(track_details, output_file=output_file)
         print(f'Successfully save track details to {output_file} from album: {album_id}')
+        return track_details
 
     def downloader_track(self, urls: list, download_names: list, threads: int = 10):
+        print(' start to download tracks '.center(100, '='))
         with ThreadPoolExecutor(threads) as t:
             for url, download_name in zip(urls, download_names):
                 t.submit(self._download_track, url=url, download_name=download_name)
 
     def _download_track(self, url: str, download_name: str):
+        time.sleep(1)
         response = requests.get(url, headers=self.headers)
         with open(f'{self.download_dir}/{download_name}.m4a', 'wb') as f:
             f.write(response.content)
@@ -280,31 +298,34 @@ class XimalayaFMCrawler(BaseXimalayaFM):
 
 
 if __name__ == '__main__':
+    # settings
     num_of_threads = 10
     my_category = '有声书'
     my_subcategory = ['文学', '经典', '成长', '社科', '商业']
-
-    my_csv_dir = r'XimalayaFM/data/'
-    my_download_dir = r'XimalayaFM/download/'
+    my_csv_dir = r'E:\DataMining\Git_Python_WebCrawler\XimalayaFM\data'  # absolute path is recommended
+    my_download_dir = r'E:\DataMining\Git_Python_WebCrawler\XimalayaFM\download'
     my_cate_output = r'data_album_basic.csv'
     my_album_output = r'data_album_details.csv'
     my_track_output = r'data_tracks.csv'
+    my_filters = {'announcer': 'single', 'paid': 'yes'}
 
-    my_filters = {'announcer': 'single', 'finished': 'yes', 'paid': 'yes'}
-
+    # crawl basic album information by category
     crawler = XimalayaFMCrawler(output_dir=my_csv_dir, download_dir=my_download_dir)
+    data_category = crawler.crawler_category(category=my_category, subcategories=my_subcategory, filters=my_filters,
+                                             threads=num_of_threads, output_file=my_cate_output)
 
-    crawler.crawler_category(category=my_category, subcategories=my_subcategory, filters=my_filters,
-                             pages=4, threads=num_of_threads, output_file=my_cate_output)
+    # crawl detailed album information
+    # data_category = pd.read_csv(my_csv_dir + my_cate_output)
+    data_albums = crawler.crawler_details(crawler.crawl_album, data_category['album_id'],
+                                          output_file=my_album_output, threads=num_of_threads)
 
-    data_category = pd.read_csv(my_csv_dir + my_cate_output)
-    crawler.crawler_album(data_category['album_id'], threads=num_of_threads, output_file=my_album_output)
+    # crawl track information
+    data_tracks = crawler.crawler_details(crawler.crawl_track, data_category['album_id'],
+                                          output_file=my_track_output, threads=num_of_threads)
 
-    crawler.crawler_track(data_category['album_id'], threads=num_of_threads, output_file=my_track_output)
-
-    data_tracks = pd.read_csv(my_csv_dir + my_track_output)
-    data_trial_tracks = data_tracks[data_tracks['track_audio'].notnull()].reset_index(drop=True)
-    data_trial_tracks['download_name'] = \
-        data_trial_tracks['album_id'].astype(str) + '_' + data_trial_tracks['track_id'].astype(str)
-    crawler.downloader_track(data_trial_tracks['track_audio'], download_names=data_trial_tracks['download_name'],
-                             threads=num_of_threads)
+    # download free (trial) tracks
+    # data_tracks = pd.read_csv(my_csv_dir + my_track_output)
+    data_trial_tracks = data_tracks[data_tracks['track_audio'] != ''].reset_index(drop=True)
+    # crawler.save_csv(data_trial_tracks.to_dict('records'), r'data_tracks_trial.csv')
+    download_names = data_trial_tracks['album_id'].astype(str) + '_' + data_trial_tracks['track_id'].astype(str)
+    crawler.downloader_track(data_trial_tracks['track_audio'], download_names=download_names, threads=num_of_threads)
